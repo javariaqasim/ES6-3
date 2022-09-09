3cba03842d419dd543b85d55457d8c43c886299c
javaria qasim <javariaqasim77@gmail.com>
2022-09-09 15:35:53 +0500

    work complete

---
+++ app.js
@@ -0,0 +1,271 @@
+let mobiles = [
+    {
+        Realme: {
+            realme9: {
+                Version: "Android 47.51%",
+                Quality: "Qualcomm SM6225 Snapdragon 680 4G (6 nm)"+"<br>"+ "	Octa-core (4x2.4 GHz Kryo 265 Gold & 4x1.9 GHz Kryo 265 Silver)"+"<br>"+"Adreno 610",
+                Price: "Rs 64,999",
+                Detail: {
+                    Processor: "Octa Core",
+                    Camera: "30MP",
+                    Battery: "5000 mAh",
+                },
+            },
+
+            realme8: {
+                Version: "Android 47.51%",
+                Quality: "Qualcomm SM6225 Snapdragon 680 4G (6 nm)"+"<br>"+ "	Octa-core (4x2.4 GHz Kryo 265 Gold & 4x1.9 GHz Kryo 265 Silver)"+"<br>"+"Adreno 610", 
+                Price: "Rs 25500",
+                Detail: {
+                    Processor: "Dual Core",
+                    Camera: "20MP",
+                    Battery: "4000 mAh",
+                },
+            },
+            realmec11: {
+                Version: "Android 47.51%",
+                Quality: "Qualcomm SM6225 Snapdragon 680 4G (6 nm)"+"<br>"+ "	Octa-core (4x2.4 GHz Kryo 265 Gold & 4x1.9 GHz Kryo 265 Silver)"+"<br>"+"Adreno 610", 
+                Price: "Rs 19000",
+                Detail: {
+                    Processor: "i Core",
+                    Camera: "10MP",
+                    Battery: "3000 mAh",
+                },
+            },
+                realmec12: {
+                    Version: "Android 47.51%",
+                    Quality: "Qualcomm SM6225 Snapdragon 680 4G (6 nm)"+"<br>"+ "	Octa-core (4x2.4 GHz Kryo 265 Gold & 4x1.9 GHz Kryo 265 Silver)"+"<br>"+"Adreno 610",
+                    Price: "Rs 24000",
+                Detail: {
+                    Processor: "Dual Core",
+                    Camera: "20MP",
+                    Battery: "4000 mAh",
+                },
+            },
+            realmec25: {
+                Version: "Android 47.51%",
+                Quality: "Qualcomm SM6225 Snapdragon 680 4G (6 nm)"+"<br>"+ "	Octa-core (4x2.4 GHz Kryo 265 Gold & 4x1.9 GHz Kryo 265 Silver)"+"<br>"+"Adreno 610",
+                Price: "Rs 30000",
+                Detail: {
+                    Processor: "Dual Core",
+                    Camera: "20MP",
+                    Battery: "4000 mAh",
+                },
+            },
+            realmec35: {
+                Version: "Android 47.51%",
+                Quality: "Qualcomm SM6225 Snapdragon 680 4G (6 nm)"+"<br>"+ "	Octa-core (4x2.4 GHz Kryo 265 Gold & 4x1.9 GHz Kryo 265 Silver)"+"<br>"+"Adreno 610",
+                Price: "Rs 35000", 
+                Detail: {
+                    Processor: "Dual Core",
+                    Camera: "20MP",
+                    Battery: "4000 mAh",
+                },
+            },
+            realmec31: {
+                Version: "Android 47.51%",
+                Quality: "Qualcomm SM6225 Snapdragon 680 4G (6 nm)"+"<br>"+ "	Octa-core (4x2.4 GHz Kryo 265 Gold & 4x1.9 GHz Kryo 265 Silver)"+"<br>"+"Adreno 610",
+                Price: "Rs: 40000",
+                Detail: {
+                    Processor: "Dual Core",
+                    Camera: "20MP",
+                    Battery: "4000 mAh",
+                },
+            },
+        },
+
+    },
+
+    {
+        vivo: {
+            Vivov20: {
+                Version: "Android 47.51%",
+                Quality: "Vivo Funtouch OS 12 (Android 12) Features List. Vivo Funtouch OS 12 is based on Android 12, and it brings almost all the new features that Google has baked into the stock version of Android 12. Apart from that, the company has also added some of its own features",
+                Price: "Rs 50000",
+                Detail: {
+                    Processor: "Octa Core",
+                    Camera: "30MP",
+                    Battery: "5000 mAh",
+                },
+            },
+            VivoY21: {
+                Version: "Android 47.51%",
+                Quality: "Vivo Funtouch OS 12 (Android 12) Features List. Vivo Funtouch OS 12 is based on Android 12, and it brings almost all the new features that Google has baked into the stock version of Android 12. Apart from that, the company has also added some of its own features",
+                Price: "Rs 70000 ",
+                Detail: {
+                    Processor: "Dual Core",
+                    Camera: "20MP",
+                    Battery: "4000 mAh",
+                },
+            },
+            VivoY33: {
+                Version: "Android 47.51%",
+                Quality: "Vivo Funtouch OS 12 (Android 12) Features List. Vivo Funtouch OS 12 is based on Android 12, and it brings almost all the new features that Google has baked into the stock version of Android 12. Apart from that, the company has also added some of its own features",
+                Price: "Rs 65000",
+                Detail: {
+                    Processor: "i Core",
+                    Camera: "10MP",
+                    Battery: "3000 mAh",
+                },
+            },
+            VivoY75: {
+                Version: "Android 47.51%",
+                Quality: "Vivo Funtouch OS 12 (Android 12) Features List. Vivo Funtouch OS 12 is based on Android 12, and it brings almost all the new features that Google has baked into the stock version of Android 12. Apart from that, the company has also added some of its own features",
+                Price: "Rs 30000",
+                Detail: {
+                    Processor: "Octa Core",
+                    Camera: "30MP",
+                    Battery: "5000 mAh",
+                },
+            },
+            VivoY12: {
+                Version: "Android 47.51%",
+                Quality: "Vivo Funtouch OS 12 (Android 12) Features List. Vivo Funtouch OS 12 is based on Android 12, and it brings almost all the new features that Google has baked into the stock version of Android 12. Apart from that, the company has also added some of its own features",
+                Price: "Rs 6544",
+                Detail: {
+                    Processor: "Octa Core",
+                    Camera: "30MP",
+                    Battery: "5000 mAh",
+                },
+            },
+            Vivos5: {
+                Version: "Android 47.51%",
+                Quality: "Vivo Funtouch OS 12 (Android 12) Features List. Vivo Funtouch OS 12 is based on Android 12, and it brings almost all the new features that Google has baked into the stock version of Android 12. Apart from that, the company has also added some of its own features",
+                Price: "Rs 98000",
+                Detail: {
+                    Processor: "Octa Core",
+                    Camera: "30MP",
+                    Battery: "5000 mAh",
+                },
+            },
+            VivoV15: {
+                Version: "Android 47.51%",
+                Quality: "Vivo Funtouch OS 12 (Android 12) Features List. Vivo Funtouch OS 12 is based on Android 12, and it brings almost all the new features that Google has baked into the stock version of Android 12. Apart from that, the company has also added some of its own features",
+                Price: "Rs 78000",
+                Detail: {
+                    Processor: "Octa Core",
+                    Camera: "30MP",
+                    Battery: "5000 mAh",
+                },
+            },
+        },
+    },
+    {
+        iphone: {
+            iphone13: {
+                Version: "iOS 41.97%",
+                Quality: "Of course, iPhones can have hardware issues, too, but they're overall higher quality. If you're buying an iPhone, you need to pick a model. Because many companies make Android devices, you have to choose both a brand and a model.",
+                Price: "Rs 30000",
+                Detail: {
+                    Processor: "Octa Core",
+                    Camera: "30MP",
+                    Battery: "5000 mAh",
+                },
+            },
+            iphone12: {
+                Version: "iOS 41.97%",
+                Quality: "Of course, iPhones can have hardware issues, too, but they're overall higher quality. If you're buying an iPhone, you need to pick a model. Because many companies make Android devices, you have to choose both a brand and a model.",
+                Price: "Rs 20000",
+                Detail: {
+                    Processor: "Octa Core",
+                    Camera: "30MP",
+                    Battery: "5000 mAh",
+                },
+            },
+            iphone11: {
+                Version: "iOS 41.97%",
+                Quality: "Of course, iPhones can have hardware issues, too, but they're overall higher quality. If you're buying an iPhone, you need to pick a model. Because many companies make Android devices, you have to choose both a brand and a model.",
+                Price: "Rs 250000",
+                Detail: {
+                    Processor: "Octa Core",
+                    Camera: "30MP",
+                    Battery: "5000 mAh",
+                },
+            },
+            iphone10: {
+                Version: "iOS 41.97%",
+                      Quality: "Of course, iPhones can have hardware issues, too, but they're overall higher quality. If you're buying an iPhone, you need to pick a model. Because many companies make Android devices, you have to choose both a brand and a model.",
+                      Price: "Rs 145000",
+                Detail: {
+                    Processor: "Octa Core",
+                    Camera: "30MP",
+                    Battery: "5000 mAh",
+                },
+            },
+            iphone7: {
+                Version: "iOS 41.97%",
+                Quality: "Of course, iPhones can have hardware issues, too, but they're overall higher quality. If you're buying an iPhone, you need to pick a model. Because many companies make Android devices, you have to choose both a brand and a model.",
+                Price: "Rs 8000",
+                Detail: {
+                    Processor: "Octa Core",
+                    Camera: "30MP",
+                    Battery: "5000 mAh",
+                },
+            },
+            iphone6: {
+                Version: "iOS 41.97%",
+                Quality: "Of course, iPhones can have hardware issues, too, but they're overall higher quality. If you're buying an iPhone, you need to pick a model. Because many companies make Android devices, you have to choose both a brand and a model.",
+                Price: "Rs 40000",
+                Detail: {
+                    Processor: "Octa Core",
+                    Camera: "30MP",
+                    Battery: "5000 mAh",
+                },
+            },
+        },
+    },
+];
+
+// ======================map () method========================
+let mobileToShow = "";
+functionToDisplay("mobileName");
+function functionToDisplay(idMobileName) {
+    mobiles.map(function (e) {
+        mobileNametoDiplay = Object.keys(e)[0];
+        mobileToShow += `<option value="${mobileNametoDiplay}"> ${mobileNametoDiplay} </Option>`;
+    });
+    document.getElementById(idMobileName).innerHTML = mobileToShow;
+}
+
+
+function functionClickTo() {
+    let mobileModel = "";
+    let mobileBrand = document.getElementById("mobileName").value;
+    mobiles.map(function (e) {
+        if (Object.keys(e)[0] == mobileBrand) {
+            let mobileModeltoDiplay = Object.keys(e[mobileBrand]);
+            mobileModeltoDiplay.map((val) => {
+                mobileModel += `<option value="${val}"> ${val} </Option>`;
+            });
+            document.getElementById("mobileModel").innerHTML = mobileModel;
+        }
+    });
+}
+
+
+// ====================filter() Method =======================
+function onValue() {
+    mobiles.filter(function (e) {
+        console.log("DATA",Object.keys(e)[0],mobileName)
+        if (Object.keys(e)[0] == mobileName.value) {
+            let mobileName = document.getElementById('mobileName')
+            let mobileModel = document.getElementById('mobileModel')
+            let resultToDisplayName = Object.keys(e[mobileName.value][mobileModel.value])
+            let resultToDisplayModel = Object.values(e[mobileName.value][mobileModel.value])
+            document.getElementById("demo").innerHTML = resultToDisplayName[0];
+            document.getElementById("demo1").innerHTML = resultToDisplayModel[0];
+            document.getElementById("demo2").innerHTML = resultToDisplayName[1];
+            document.getElementById("demo3").innerHTML = resultToDisplayModel[1];
+            document.getElementById("demo4").innerHTML = resultToDisplayName[2];
+            document.getElementById("demo5").innerHTML = resultToDisplayModel[2];
+            document.getElementById("demo6").innerHTML = resultToDisplayName[3];
+            document.getElementById("demo7").innerHTML = resultToDisplayModel[3].Processor;
+            document.getElementById("demo8").innerHTML = resultToDisplayModel[3].Camera;
+            document.getElementById("demo9").innerHTML = resultToDisplayModel[3].Battery;
+        }
+    }
+    )
+};
+functionClickTo()
+
+
